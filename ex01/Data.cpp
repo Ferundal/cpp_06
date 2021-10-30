44337fd4401ab747e37e163ee5bcc5abc9fef4d0
cjettie <ferundal@gmail.com>
2021-10-30 15:27:20 +0300

    little fixe ex00, ex01 almost done

--- ex01/Data.cpp
+++ ex01/Data.cpp
@@ -1,17 +1,26 @@
 #include "Data.hpp"
-Data::Data(void ) {
+Data::Data(void ) : secret("I love seals") {
 
 }
 Data::Data(const Data& origin) {
-
+	*this = origin;
 }
-Data::Data& operator =(const Data& origin) {
-
+Data& Data::operator =(const Data& origin) {
+	secret = origin.secret;
+	return (*this);
 }
 
 Data::~Data(void ) {
 
 }
 
-uintptr_t	serialize(Data* ptr);
-Data*		deserialize(uintptr_t raw);
+void Data::TellSecret(void) {
+	std::cout << secret << std::endl;
+}
+
+uintptr_t	serialize(Data* ptr) {
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+Data*		deserialize(uintptr_t raw) {
+	return (reinterpret_cast<Data*>(raw));
+}
---
+++ ex02/Base.cpp
@@ -0,0 +1,13 @@
+#include "Base.hpp"
+Base::Base(void ) : secret("I love seals") {
+
+}
+Base::Base(const Data& origin) {
+}
+
+Base& Base::operator =(const Base& origin) {
+}
+
+Base::~Base(void ) {
+
+}
